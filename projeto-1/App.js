1205c211ad1efd5247797761261ade48aeaa87bb
Rafael <ra.facavalcante@hotmail.com>
2021-07-29 13:15:17 -0300

    App: inserindo componente header

--- projeto-1/App.js
+++ projeto-1/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
-  
+import Header from './components/Header'
 export default class App extends React.Component {
 
 
