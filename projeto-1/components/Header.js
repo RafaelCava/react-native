7d9bb1ef01a89f1fc2c28be89e1d1f9ac11e98b7
Rafael <ra.facavalcante@hotmail.com>
2021-07-29 13:19:45 -0300

    criando componente Header

---
+++ projeto-1/components/Header.js
@@ -0,0 +1,13 @@
+import { Text } from 'react-native'
+import React from 'react';
+const Header = () =>{
+  return (
+    <>
+      <Text>Hello</Text>
+      <Text>Hello</Text>
+      <Text>Hello</Text>
+    </>
+  )
+}
+
+export default Header
